39a7f50072d12eb0ea48302e271c27744239c223
Ashley Newman <ashnew@umich.edu>
2020-11-18 17:46:22 -0500

    database stuff is working wooooooooooo

--- final.py
+++ final.py
@@ -2,9 +2,6 @@ import json
 import unittest
 import os
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
 import sqlite3
 
 client_id = "m8K0vXjoUNXwMo4AfkpZnw"
@@ -12,24 +9,8 @@ API_KEY = "U-Y8QD3RL11wbcrmj9w3Yt3i_vFz97mdYqynngNtmGCFGZpmcdFRkGrMMztgvFwtvo7YR
 headers = {'Authorization': 'Bearer %s' % API_KEY}
 base_url = "https://api.yelp.com/v3"
 
-import requests
-from pprint import pprint
-#test hello
-# locationUrlFromLatLong = "https://developers.zomato.com/api/v2.1/cities?lat=28&lon=77"
-# header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": API_KEY}
-# response = requests.get(locationUrlFromLatLong, headers=header)
-# pprint(response.json())
-
-
-# search = BASE_URL + "/search?entity_id=280&entity_type=city&count=4&sort=rating"
-# header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": API_KEY}
-# r = requests.get(search, headers=header)
-# response = r.json()
-# pprint(response)
-# name_restaurant = response["restaurants"][0][]
-def setUpDatabase(db_name):
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_name)
+def create_connection(database):
+    conn = sqlite3.connect(database)
     cur = conn.cursor()
     return cur, conn
 
@@ -40,8 +21,8 @@ def city_restaurant_data(city_name, term_name, off):
     info = json.loads(req.text)
     return info
     
-def table_set_up(data, conn, cur, city_name):
-    cur.execute(f'CREATE TABLE IF NOT EXISTS {city_name}')
+def table_set_up(data, city_name, cur, conn):
+    cur.execute(f'CREATE TABLE IF NOT EXISTS {city_name} (id_num TEXT, name TEXT, food_type TEXT, rating INTEGER, longitude INTEGER, latitude INTEGER)')
     restaurant_list = data['businesses']
     for restaurant in restaurant_list:
         id_num = restaurant['id']
@@ -50,8 +31,9 @@ def table_set_up(data, conn, cur, city_name):
         longitude = restaurant['coordinates']['longitude']
         latitude = restaurant['coordinates']['latitude']
         type_of_food = restaurant['categories'][0]['title']
-        cur.execute(f'INSERT INTO {city_name} (id_num, name, food_type, rating, longitude, latitude) VALUES ({id_num}, {name}, {type_of_food}, {rating}, {longitude}, {latitude})')
+        cur.execute(f'INSERT INTO {city_name} (id_num, name, food_type, rating, longitude, latitude) VALUES (?,?,?,?,?,?)', (id_num, name, type_of_food, rating, longitude, latitude))
     conn.commit()
+
 def top_cities():
     url = 'https://travel.usnews.com/rankings/best-foodie-destinations-in-the-usa/'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'}
@@ -71,60 +53,18 @@ def top_cities():
 
     return d
 
-def wiki_data():
-    soup = BeautifulSoup(requests.get('https://en.wikipedia.org/wiki/List_of_North_American_metropolitan_areas_by_population').text, 'html.parser')
-
-    table = soup.find("table",{"class":"wikitable sortable"})
-    body = table.find('tbody')
-    table_rows = body.find_all('tr')
-    d = []
-
-    for tr in table_rows:
-        td = tr.find_all('td')
-        row = [tp.text.strip() for tp in td]
-        d.append(row)   
-            
-    df = pd.DataFrame(d) 
-    df = df[[1, 2, 3]]
-    df.head()
-
-    h = []
-    header_cols = table.find_all('th')
-    for col in header_cols:
-        results_str = col.text.strip()
-        split = results_str.split('\n')
-        h.append(split[0])  
-    
-    df.columns = h[1:]
-    df = df.iloc[1:]
-
-    is_us = df['Country'] == 'United States'
-    us_cities = df[is_us]
-    list_of_cities = us_cities['Metropolitan area'].to_list()
-
-    return list_of_cities
-    #comment to test push
 
 def main():
-    cur, conn = setUpDatabase('db_test.db')
-    #cities = wiki_data()
-    #print(cities)
+    cur, conn = create_connection("db_test.db")
+
     i = 0
     while i < 76:
-        table_set_up(city_restaurant_data("New York City", "restaurant", i), conn, cur, "New York City")
+        rest_info = city_restaurant_data("New York City", "restaurant", i)
+        table_set_up(rest_info, "New_York_City", cur, conn)
         i += 25
+
     ls = top_cities()
     print(ls)
-#average income of cities compared to price of most popular restaurant in that city
-#income of area in NYC compared to most popular restaurant (by rating) for that area
-#major 5 cities incomes compared to 20 most popular restaurants for that city
-#table for cities- name, income, population
-#table for restaurants- name, location, rating, price
-#database- city name, restaurant name, income, population, restaurant rating, price of restaurant
-#comparing the top rated restaurants in the most popular cities to see if the neighborhood's income is related to the rating and price of the modt popular restaurants
-#our data will either support this claim or we will be surprised by the results
-#calculate average rating per area and see which city has the highest rating on average
-#can switch to yelp
 
 if __name__ == "__main__":
     main()
