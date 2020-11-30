77e1d224f5564f7a5a404703fde74f11b41a63b4
Ashley Newman <ashnew@umich.edu>
2020-11-30 17:59:30 -0500

    two files now- one with db stuff one with calcs and visualizations

---
+++ calcs_vis.py
@@ -0,0 +1,230 @@
+import unittest
+import sqlite3
+import json
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def visualization_1(cur):
+    avg1 = []
+    avg1_5 = []
+    avg2 = []
+    avg2_5 = []
+    avg3 = []
+    cur.execute('SELECT city, total_score FROM HappyData JOIN WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp < 10')
+    temp_ones = cur.fetchall()
+    for i in temp_ones:
+        if i[1] < 50:
+            avg1.append(i[0])
+        elif i[1] > 70:
+            avg2.append(i[0])
+        else:
+            avg1_5.append(i[0])
+    cur.execute('SELECT city, total_score FROM HappyData JOIN WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp >= 10 AND WeatherData.avg_temp <= 21')
+    temp_twos = cur.fetchall()
+    for i in temp_twos:
+        if i[1] < 50:
+            avg1_5.append(i[0])
+        elif i[1] > 70:
+            avg2_5.append(i[0])
+        else:
+            avg2.append(i[0]) 
+    cur.execute('SELECT city, total_score FROM HappyData JOIN WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp > 21')
+    temp_threes = cur.fetchall()
+    for i in temp_threes:
+        if i[1] < 50:
+            avg2.append(i[0])
+        elif i[1] > 70:
+            avg3.append(i[0])
+        else:
+            avg2_5.append(i[0])
+    x_city = []
+    y_rank = []
+    for i in avg1:
+        x_city.append(i)
+        y_rank.append("1")
+    for i in avg1_5:
+        x_city.append(i)
+        y_rank.append("1.5")
+    for i in avg2:
+        x_city.append(i)
+        y_rank.append("2")
+    for i in avg2_5:
+        x_city.append(i)
+        y_rank.append("2.5")
+    for i in avg3:
+        x_city.append(i)
+        y_rank.append("3")
+    fig, ax = plt.subplots()
+    ax.bar(x_city, y_rank)
+    ax.set_xlabel('city')
+    ax.set_ylabel('rank')
+    ax.set_title('Bargraph of the Average Temperature to Total Happiness Score Ranking Per City')
+    fig.savefig('test1.png')
+    plt.show()
+
+def visualization_2(cur):
+    avg1 = []
+    avg1_5 = []
+    avg2 = []
+    avg2_5 = []
+    avg3 = []
+    cur.execute('SELECT city, avg_precipitation FROM HappyData, WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp < 10')
+    temp_ones = cur.fetchall()
+    for i in temp_ones:
+        if i[1] > 900:
+            avg1.append(i[0])
+        elif i[1] < 170:
+            avg2.append(i[0])
+        else:
+            avg1_5.append(i[0])
+    cur.execute('SELECT city, avg_precipitation FROM HappyData, WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp >= 10 AND WeatherData.avg_temp <= 21')
+    temp_twos = cur.fetchall()
+    for i in temp_twos:
+        if i[1] > 900:
+            avg1_5.append(i[0])
+        elif i[1] < 170:
+            avg2_5.append(i[0])
+        else:
+            avg2.append(i[0]) 
+    cur.execute('SELECT city, avg_precipitation FROM HappyData, WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp > 21')
+    temp_threes = cur.fetchall()
+    for i in temp_threes:
+        if i[1] > 900:
+            avg2.append(i[0])
+        elif i[1] < 170:
+            avg3.append(i[0])
+        else:
+            avg2_5.append(i[0])
+    x_city = []
+    y_rank = []
+    for i in avg1:
+        x_city.append(i)
+        y_rank.append("1")
+    for i in avg1_5:
+        x_city.append(i)
+        y_rank.append("1.5")
+    for i in avg2:
+        x_city.append(i)
+        y_rank.append("2")
+    for i in avg2_5:
+        x_city.append(i)
+        y_rank.append("2.5")
+    for i in avg3:
+        x_city.append(i)
+        y_rank.append("3")
+    fig, ax = plt.subplots()
+    ax.bar(x_city, y_rank)
+    ax.set_xlabel('city')
+    ax.set_ylabel('rank')
+    ax.set_title('Bargraph of the Average Temperature to Average Precipitation Ranking Per City')
+    fig.savefig('test2.png')
+    plt.show()
+
+def visualization_3(cur):
+    x = []
+    y = []
+    cur.execute('SELECT avg_temp FROM WeatherData')
+    temp_x = cur.fetchall()
+    for i in temp_x:
+        x.append(i[0])
+    cur.execute('SELECT total_score FROM HappyData')
+    temp_y = cur.fetchall()
+    for i in temp_y:
+        y.append(i[0])
+    fig, ax = plt.subplots()
+    ax.scatter(x, y)
+    ax.set_xlabel('average temperature in degrees Celsius')
+    ax.set_ylabel('total happiness score')
+    ax.set_title('Scatterplot of the Average Temperature vs Total Happiness Score for Each City')
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    plt.plot(x, p(x), "r-")
+    r = np.corrcoef(x, y)
+    print("correlation coefficient: " + str(r[0,1]))
+    fig.savefig('test3.png')
+    plt.show()
+
+def visualization_4(cur):
+    x = []
+    y = []
+    cur.execute('SELECT avg_precipitation FROM WeatherData')
+    temp_x = cur.fetchall()
+    for i in temp_x:
+        x.append(i[0])
+    cur.execute('SELECT total_score FROM HappyData')
+    temp_y = cur.fetchall()
+    for i in temp_y:
+        y.append(i[0])
+    fig, ax = plt.subplots()
+    ax.scatter(x, y)
+    ax.set_xlabel('average precipitation in mm')
+    ax.set_ylabel('total happiness score')
+    ax.set_title('Scatterplot of the Average Temperature vs Total Happiness Score for Each City')
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    plt.plot(x, p(x), "r-")
+    r = np.corrcoef(x, y)
+    print("correlation coefficient: " + str(r[0,1]))
+    fig.savefig('test4.png')
+    plt.show()
+
+def visualization_5(cur):
+    cur.execute('SELECT avg_temp FROM WeatherData')
+    column1 = cur.fetchall()
+    x = stats.zscore(column1)
+    cur.execute('SELECT avg_precipitation FROM WeatherData')
+    column2 = cur.fetchall()
+    y = stats.zscore(column2)
+    zipped = list(zip(x, y))
+    avg = []
+    for i in zipped:
+        avg.append((i[0][0] + i[1][0]) / 2)
+    cur.execute('SELECT total_score FROM HappyData')
+    tot_score = []
+    temp_y = cur.fetchall()
+    for i in temp_y:
+        tot_score.append(i[0])
+    fig, ax = plt.subplots()
+    ax.scatter(avg, tot_score)
+    ax.set_xlabel('average weather index')
+    ax.set_ylabel('total happiness score')
+    ax.set_title('Scatterplot of the weather index vs happiness score for each city')
+    z = np.polyfit(avg, tot_score, 1)
+    p = np.poly1d(z)
+    plt.plot(avg, p(avg), "r-")
+    r = np.corrcoef(avg, tot_score)
+    print("correlation coefficient: " + str(r[0,1]))
+    fig.savefig('test5.png')
+    plt.show()
+
+def visualization_6(cur):
+    cur.execute('SELECT avg_temp, avg_precipitation, city, total_score FROM WeatherData, HappyData JOIN Locations WHERE Locations.id = WeatherData.location')
+    x = cur.fetchall()
+    print(x)
+
+
+
+
+
+def main():
+    cur, conn = setUpDatabase('attempt3.db')
+    # visualization_1(cur)
+    # visualization_2(cur) 
+    # visualization_3(cur)
+    # visualization_4(cur)
+    # visualization_5(cur)
+    visualization_6(cur)
+    
+
+
+if __name__ == "__main__":
+    main()
--- final.py
+++ final.py
@@ -8,6 +8,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
+import calcs_vis
 
 location_lat_long_api_key = "MQosMrFCrMn14jS6h32hinuTQWrHDd5q"
 weather_api_key = "IYyo8JaZe0W8MznnblAr2cRPpDeeTQGa"
@@ -54,8 +55,7 @@ def get_weather_data(location):
         #gets average of month in celcius
         temp_string_celcius = month['tavg']
         percip = month['prcp']
-        #pressure = month['pres']
-        #sunshine = month['tsun']
+  
         weather_data[month_num_string] = (temp_string_celcius, percip) #, pressure, sunshine
   
     
@@ -66,18 +66,14 @@ def weather_table(data, cur, conn, location):
     cur.execute(f'CREATE TABLE IF NOT EXISTS WeatherData (location INTEGER, avg_temp INTEGER, avg_precipitation INTEGER)') #, avg_pressure INTEGER, avg_hours_sunshine INTEGER
     temp = 0
     percip = 0
-    #pressure = 0.0
-    #sun = 0
+  
     for key in data:
         temp += int(data[key][0])
         percip += int(data[key][1])
-        #pressure += float(data[key][2])
-        #sun += int(data[key][3])
+ 
     avg_temp = temp / 12
     avg_percip = percip / 12
-    #avg_pressure = pressure / 12
-    #avg_sunshine = sun / 12
-    #, avg_pressure, avg_hours_sunshine
+
     cur.execute(f'INSERT INTO WeatherData (location, avg_temp, avg_precipitation) VALUES (?,?,?)', (location, avg_temp, avg_percip)) #, avg_pressure, avg_sunshine
     conn.commit()
 
@@ -90,16 +86,6 @@ def get_website_data(i):
     soup = BeautifulSoup(r.text, "html.parser")
     table = soup.find("table",{"class":"cardhub-edu-table center-aligned sortable"})
 
-    #gets the 6 titles for columns in data table
-    # headers = [] 
-    # head = table.find("thead")
-    # tr = head.find("tr")
-    # head_tags = tr.find_all("th")
-    # for tag in head_tags:
-    #     headers.append(tag.text.strip())
-    
-    # state_ids = []
-    # data = []
     body = table.find("tbody")
     row_tags = body.find_all("tr")
 
@@ -107,10 +93,7 @@ def get_website_data(i):
     td_tags = tr_tag.find_all("td")
     overall_rank = int(td_tags[0].text.strip())
     city_name = td_tags[1].text.strip()
-        #---------
-    # temp = city_name.split(",")
-    # state_ids.append((temp[0], temp[1]))
-        #--------
+
     total_score = float(td_tags[2].text.strip())
     emotional_and_pysical_score = int(td_tags[3].text.strip())
     income_and_employment_score = int(td_tags[4].text.strip())
@@ -123,204 +106,6 @@ def make_website_table(data, cur, conn):
     cur.execute('CREATE TABLE IF NOT EXISTS HappyData (overall_rank INTEGER, city TEXT, total_score INTEGER, well_being_rank INTEGER, income_employment_rank INTEGER, community_environment_rank INTEGER)')
     cur.execute('INSERT INTO HappyData (overall_rank, city, total_score, well_being_rank, income_employment_rank, community_environment_rank)  VALUES (?,?,?,?,?,?)', (data[0], data[1], data[2], data[3], data[4], data[5]))
     conn.commit()
-
-def visualization_1(cur):
-    avg1 = []
-    avg1_5 = []
-    avg2 = []
-    avg2_5 = []
-    avg3 = []
-    cur.execute('SELECT city, total_score FROM HappyData JOIN WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp < 10')
-    temp_ones = cur.fetchall()
-    for i in temp_ones:
-        if i[1] < 50:
-            avg1.append(i[0])
-        elif i[1] > 70:
-            avg2.append(i[0])
-        else:
-            avg1_5.append(i[0])
-    cur.execute('SELECT city, total_score FROM HappyData JOIN WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp >= 10 AND WeatherData.avg_temp <= 21')
-    temp_twos = cur.fetchall()
-    for i in temp_twos:
-        if i[1] < 50:
-            avg1_5.append(i[0])
-        elif i[1] > 70:
-            avg2_5.append(i[0])
-        else:
-            avg2.append(i[0]) 
-    cur.execute('SELECT city, total_score FROM HappyData JOIN WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp > 21')
-    temp_threes = cur.fetchall()
-    for i in temp_threes:
-        if i[1] < 50:
-            avg2.append(i[0])
-        elif i[1] > 70:
-            avg3.append(i[0])
-        else:
-            avg2_5.append(i[0])
-    x_city = []
-    y_rank = []
-    for i in avg1:
-        x_city.append(i)
-        y_rank.append("1")
-    for i in avg1_5:
-        x_city.append(i)
-        y_rank.append("1.5")
-    for i in avg2:
-        x_city.append(i)
-        y_rank.append("2")
-    for i in avg2_5:
-        x_city.append(i)
-        y_rank.append("2.5")
-    for i in avg3:
-        x_city.append(i)
-        y_rank.append("3")
-    fig, ax = plt.subplots()
-    ax.bar(x_city, y_rank)
-    ax.set_xlabel('city')
-    ax.set_ylabel('rank')
-    ax.set_title('Bargraph of the Average Temperature to Total Happiness Score Ranking Per City')
-    fig.savefig('test1.png')
-    plt.show()
-
-def visualization_2(cur):
-    avg1 = []
-    avg1_5 = []
-    avg2 = []
-    avg2_5 = []
-    avg3 = []
-    cur.execute('SELECT city, avg_precipitation FROM HappyData, WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp < 10')
-    temp_ones = cur.fetchall()
-    for i in temp_ones:
-        if i[1] > 900:
-            avg1.append(i[0])
-        elif i[1] < 170:
-            avg2.append(i[0])
-        else:
-            avg1_5.append(i[0])
-    cur.execute('SELECT city, avg_precipitation FROM HappyData, WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp >= 10 AND WeatherData.avg_temp <= 21')
-    temp_twos = cur.fetchall()
-    for i in temp_twos:
-        if i[1] > 900:
-            avg1_5.append(i[0])
-        elif i[1] < 170:
-            avg2_5.append(i[0])
-        else:
-            avg2.append(i[0]) 
-    cur.execute('SELECT city, avg_precipitation FROM HappyData, WeatherData WHERE HappyData.overall_rank = WeatherData.location + 1 AND WeatherData.avg_temp > 21')
-    temp_threes = cur.fetchall()
-    for i in temp_threes:
-        if i[1] > 900:
-            avg2.append(i[0])
-        elif i[1] < 170:
-            avg3.append(i[0])
-        else:
-            avg2_5.append(i[0])
-    x_city = []
-    y_rank = []
-    for i in avg1:
-        x_city.append(i)
-        y_rank.append("1")
-    for i in avg1_5:
-        x_city.append(i)
-        y_rank.append("1.5")
-    for i in avg2:
-        x_city.append(i)
-        y_rank.append("2")
-    for i in avg2_5:
-        x_city.append(i)
-        y_rank.append("2.5")
-    for i in avg3:
-        x_city.append(i)
-        y_rank.append("3")
-    fig, ax = plt.subplots()
-    ax.bar(x_city, y_rank)
-    ax.set_xlabel('city')
-    ax.set_ylabel('rank')
-    ax.set_title('Bargraph of the Average Temperature to Average Precipitation Ranking Per City')
-    fig.savefig('test2.png')
-    plt.show()
-
-def visualization_3(cur):
-    x = []
-    y = []
-    cur.execute('SELECT avg_temp FROM WeatherData')
-    temp_x = cur.fetchall()
-    for i in temp_x:
-        x.append(i[0])
-    cur.execute('SELECT total_score FROM HappyData')
-    temp_y = cur.fetchall()
-    for i in temp_y:
-        y.append(i[0])
-    fig, ax = plt.subplots()
-    ax.scatter(x, y)
-    ax.set_xlabel('average temperature in degrees Celsius')
-    ax.set_ylabel('total happiness score')
-    ax.set_title('Scatterplot of the Average Temperature vs Total Happiness Score for Each City')
-    z = np.polyfit(x, y, 1)
-    p = np.poly1d(z)
-    plt.plot(x, p(x), "r-")
-    r = np.corrcoef(x, y)
-    print("correlation coefficient: " + str(r[0,1]))
-    fig.savefig('test3.png')
-    plt.show()
-
-def visualization_4(cur):
-    x = []
-    y = []
-    cur.execute('SELECT avg_precipitation FROM WeatherData')
-    temp_x = cur.fetchall()
-    for i in temp_x:
-        x.append(i[0])
-    cur.execute('SELECT total_score FROM HappyData')
-    temp_y = cur.fetchall()
-    for i in temp_y:
-        y.append(i[0])
-    fig, ax = plt.subplots()
-    ax.scatter(x, y)
-    ax.set_xlabel('average precipitation in mm')
-    ax.set_ylabel('total happiness score')
-    ax.set_title('Scatterplot of the Average Temperature vs Total Happiness Score for Each City')
-    z = np.polyfit(x, y, 1)
-    p = np.poly1d(z)
-    plt.plot(x, p(x), "r-")
-    r = np.corrcoef(x, y)
-    print("correlation coefficient: " + str(r[0,1]))
-    fig.savefig('test4.png')
-    plt.show()
-
-def visualization_5(cur):
-    cur.execute('SELECT avg_temp FROM WeatherData')
-    column1 = cur.fetchall()
-    x = stats.zscore(column1)
-    cur.execute('SELECT avg_precipitation FROM WeatherData')
-    column2 = cur.fetchall()
-    y = stats.zscore(column2)
-    zipped = list(zip(x, y))
-    avg = []
-    for i in zipped:
-        avg.append((i[0][0] + i[1][0]) / 2)
-    cur.execute('SELECT total_score FROM HappyData')
-    tot_score = []
-    temp_y = cur.fetchall()
-    for i in temp_y:
-        tot_score.append(i[0])
-    fig, ax = plt.subplots()
-    ax.scatter(avg, tot_score)
-    ax.set_xlabel('average weather index')
-    ax.set_ylabel('total happiness score')
-    ax.set_title('Scatterplot of the weather index vs happiness score for each city')
-    z = np.polyfit(avg, tot_score, 1)
-    p = np.poly1d(z)
-    plt.plot(avg, p(avg), "r-")
-    r = np.corrcoef(avg, tot_score)
-    print("correlation coefficient: " + str(r[0,1]))
-    fig.savefig('test5.png')
-    plt.show()
-
-def visualization_6(cur):
-    cur.execute('SELECT avg_temp, avg_precipitation, city, total_score FROM WeatherData, HappyData JOIN Locations WHERE Locations.id = WeatherData.location')
-    x = cur.fetchall()
-    print(x)
 
 def get_start_index(cur, conn):
     cur.execute('CREATE TABLE IF NOT EXISTS HappyData (overall_rank INTEGER, city TEXT, total_score INTEGER, well_being_rank INTEGER, income_employment_rank INTEGER, community_environment_rank INTEGER)')
@@ -348,12 +133,7 @@ def main():
     while start < stop:
         current_index = get_start_index(cur, conn)
         if current_index == -1:
-            # visualization_1(cur)
-            # visualization_2(cur)
-            # visualization_3(cur)
-            # visualization_4(cur)
-            # visualization_5(cur)
-            visualization_6(cur)
+            calcs_vis.main()
             break
 
         location = get_website_data(current_index) #list of current location's data from website table
