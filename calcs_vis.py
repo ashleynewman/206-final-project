8121c87b39904c8c94d9c03a455b8f9179a265ac
Ashley Newman <ashnew@umich.edu>
2020-12-04 16:37:46 -0500

    python files check

--- calcs_vis.py
+++ calcs_vis.py
@@ -10,24 +10,39 @@ from numpy import percentile
 import csv
 
 def setUpDatabase(db_name):
+    '''This function takes in the name of the database and creates a connection and cursor to it.
+    It returns the cursor and connection.'''
+
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
 
 def calculation(cur):
-    cur.execute('SELECT temperature FROM WeatherData JOIN Temperatures ON Temperatures.id = WeatherData.average_temp_id')
+    '''This function takes in the cursor for finalData1 as input. Uses SQL SELECT...FROM...JOIN 
+    statement to find where temperature that results from joining the WeatherData and Temperature
+     tables together. This list of temperatures is extracted with the fetchall() command. The uses 
+     SELECT statement to extract a list of the happiness rating scores from the HappyData table. 
+     Then performs a SELECT statement to extract a list of average precipitation figures from the 
+     WeatherData table. Finally, extracts a list of city names using a SELECT statement to extract 
+     city names from the HappyData table. Returns a tuple of lists for temperature, happiness scores,
+    precipitation, and city names.'''
+
+    cur.execute('SELECT temperature FROM WeatherData JOIN Temperatures ON Temperatures.id = WeatherData.average_temperature_id')
     temperatures = cur.fetchall()
     cur.execute('SELECT total_score FROM HappyData')
     happy_scores = cur.fetchall()
-    cur.execute('SELECT average_precipitation_id FROM WeatherData')
+    cur.execute('SELECT average_precipitation FROM WeatherData')
     precip = cur.fetchall()
     cur.execute('SELECT city FROM HappyData')
     city_names = cur.fetchall()
     return temperatures, happy_scores, precip, city_names
 
 def write_csv(x, y, names_city, file_name, headers):
-    
+    '''This function takes in x and y axis data as lists, a list of city names, the csv file to 
+    write to, and the headers to include at the top of the csv file. Writes to the csv file all of the
+    information passed in from the x data, y data and city lists and does not return anything.'''
+
     file = open(file_name, mode='w', newline='', encoding="utf8")
     writer = csv.writer(file, delimiter=',')
     writer.writerow(headers)
@@ -36,6 +51,11 @@ def write_csv(x, y, names_city, file_name, headers):
     file.close() 
 
 def visualization1(temp, happy, city_names):
+    '''This function takes in a list of temperatures, a list of happiness scores, and a list of city 
+    names as inputs. Calls the write_csv function to write to the csv the data from these lists, and 
+    then plots the data on a matplotlib plot. The figure is saved as ‘v1.png’ and does not return 
+    anything.'''
+
     x = []
     y = []
     names_city = []
@@ -46,7 +66,7 @@ def visualization1(temp, happy, city_names):
     for i in city_names:
         names_city.append(i[0])
 
-    write_csv(x, y, names_city, "file1.csv", ['City', 'Temperature', 'Happiness Score'])
+    write_csv(x, y, names_city, "tempHappy.csv", ['City', 'Temperature', 'Happiness Score'])
     fig, ax = plt.subplots()
     ax.scatter(x, y, color='#32db84')
     ax.set_xlabel('temperature in degrees Celcius')
@@ -56,7 +76,7 @@ def visualization1(temp, happy, city_names):
     p = np.poly1d(z)
     plt.plot(x, p(x), "r-")
     r = np.corrcoef(x, y)
-    print("correlation coefficient: " + str(r[0,1]))
+    print("correlation coefficient for temperature and happiness scatterplot: " + str(r[0,1]))
 
     s1 = sorted(x)
     s2 = sorted(y)
@@ -64,10 +84,14 @@ def visualization1(temp, happy, city_names):
     avg2 = (s2[0] + s2[-1]) / 2
     plt.axvline(avg1)
     plt.axhline(avg2)
-    fig.savefig('v1.png')
+    fig.savefig('tempHappyScatterplot.png')
     plt.show()
 
 def visualization2(precip, happy, city_names):
+    '''This function takes in a list of precipitation data, a list of happiness scores, and a list of
+     city names as inputs. Calls the write_csv function to write to the csv the data from these lists,
+      and then plots the data on a matplotlib plot. The figure is saved as ‘v2.png’ and does not return anything.'''
+
     x = []
     y = []
     names_city = []
@@ -78,7 +102,7 @@ def visualization2(precip, happy, city_names):
     for i in city_names:
         names_city.append(i[0])
 
-    write_csv(x, y, names_city, "file2.csv", ['City', 'Precipitation', 'Happiness Score'])
+    write_csv(x, y, names_city, "precipHappy.csv", ['City', 'Precipitation', 'Happiness Score'])
 
     fig, ax = plt.subplots()
     ax.scatter(x, y, color='#7303fc')
@@ -89,7 +113,7 @@ def visualization2(precip, happy, city_names):
     p = np.poly1d(z)
     plt.plot(x, p(x), "r-")
     r = np.corrcoef(x, y)
-    print("correlation coefficient: " + str(r[0,1]))
+    print("correlation coefficient for precipitation and happiness scatterplot: " + str(r[0,1]))
 
     s1 = sorted(x)
     s2 = sorted(y)
@@ -98,10 +122,16 @@ def visualization2(precip, happy, city_names):
     plt.axvline(avg1)
     plt.axhline(avg2)
 
-    fig.savefig('v2.png')
+    fig.savefig('precipHappyScatterplot.png')
     plt.show()
 
 def box_and_wiskers(data, x_label, fig_name, title, csv_name):
+    '''This function takes in a list of the data of interest we want to plot, the x_label for the plot, 
+    the name of the figure, title, and the name of the csv file that is desired to save the data into.
+     Creates a box-and-wiskers plot, which includes the quartiles for the data and the iqr as well. It
+      then writes to the csv file the Min, q1, Median, Q3, Max, and IQR data for the data passed into 
+      the the function. The function returns nothing.'''
+
     good_data = []
     for i in data:
         good_data.append(i[0])
@@ -127,13 +157,19 @@ def box_and_wiskers(data, x_label, fig_name, title, csv_name):
 
 
 def main():
-    cur, conn = setUpDatabase('finalData1.db')
+    '''The main() function akes in no inputs. It calls the calculations function to return data for 
+    temperature, happiness scores, precipitation, and city names, and uses these to call the 
+    visualization1() and visualization2() functions. Finally, the box_and_wiskers() function is called
+     on precipitation, temperatures and happiness scores individually so we can see the box-and-whiskers
+      plots for each of these lists of data. Main()returns nothing.'''
+
+    cur, conn = setUpDatabase('finalProjectDatabase.db')
     temperatures, happy_scores, precipitation, city_names = calculation(cur)
     visualization1(temperatures, happy_scores, city_names)
     visualization2(precipitation, happy_scores, city_names)
-    box_and_wiskers(precipitation, 'Precipitation (mm)', 'vis3.png', 'Boxplot Precipitation', 'precip.csv')
-    box_and_wiskers(temperatures, 'Temperature (Deg. Celcius)', 'vis4.png', 'Boxplot Temperature', 'temp.csv')
-    box_and_wiskers(happy_scores, 'Happiness Scores', 'vis5.png', 'Boxplot Happiness Scores', 'happy.csv')
+    box_and_wiskers(precipitation, 'Precipitation (mm)', 'precipBoxplot.png', 'Boxplot Precipitation', 'precip.csv')
+    box_and_wiskers(temperatures, 'Temperature (Deg. Celcius)', 'tempBoxplot.png', 'Boxplot Temperature', 'temp.csv')
+    box_and_wiskers(happy_scores, 'Happiness Scores', 'happyBoxplot.png', 'Boxplot Happiness Scores', 'happy.csv')
 
     
 
--- final.py
+++ final.py
@@ -10,37 +10,42 @@ weather_api_key = "IYyo8JaZe0W8MznnblAr2cRPpDeeTQGa"
 
 
 def create_connection(database):
+    '''This function takes in the name of the database. It makes a 
+    connection to the cursor using the name of the database given. 
+    It returns the connection variable and cursor variable to allow 
+    access into the database.'''
     conn = sqlite3.connect(database)
     cur = conn.cursor()
     return cur, conn
 
 def lat_long_table(cur, conn, lat, long, place):
-    cur.execute('CREATE TABLE IF NOT EXISTS LatLongData (id INTEGER, latitude INTEGER, longitude INTEGER)')
-    cur.execute('INSERT INTO LatLongData (id, latitude, longitude) VALUES (?,?,?)', (place, lat, long))
+    '''This function takes in the cursor, connection, latitude,
+    longitude, and name for a specific city. It creates a Latitude/Longitude
+    table in the database and inserts the city, latitude, and longitude. It returns
+    nothing. '''
+    cur.execute('CREATE TABLE IF NOT EXISTS LatLongData (city_name INTEGER, latitude INTEGER, longitude INTEGER)')
+    cur.execute('INSERT INTO LatLongData (city_name, latitude, longitude) VALUES (?,?,?)', (place, lat, long))
 
 def get_weather_data(cur, conn, location):
+    '''This function takes in the cursor, connection, and city name. 
+    First request the geocoding API and include the city name in the request.
+    Use json to extract the latitude and longitude from this. Then call the lat_long_table function to
+    create the latitude/longitude table. Now, call the weather API in order to get a json file of the 
+    weather information per month. Add each month's data to a dictionary (key = month number, value = tuple of temperature and 
+    precipitation for that month). Return that dictionary. '''
   
-    #below, the lat_long_info is a json object with the lat and long coordinates inside
-    #the only thing with the url that needs to change is the "location" parameter I guess
-    #to whatever city name we pass in
-    #otherwise i wouldn't change this url
-    
-    mapquest_url = f"http://open.mapquestapi.com/geocoding/v1/address?key={location_lat_long_api_key}&location={location}" #&location=Boston, MA
+    mapquest_url = f"http://open.mapquestapi.com/geocoding/v1/address?key={location_lat_long_api_key}&location={location}" 
     req2 = requests.get(mapquest_url)
     lat_long_info = json.loads(req2.text)
    
     #below gets dictionary of just the lat and long for the city
     results = lat_long_info['results'][0]['locations'][0]['latLng']
-    #below are the lat and long strings, gotta keep them as strings so we can pass them
-    #into the api for weather data
+    #below are the lat and long strings
     lat_str = results['lat']
     long_str = results['lng']
     lat_long_table(cur, conn, lat_str, long_str, location)
-    #the "weather_info" json object should return monthly weather data in celcius 
-    #given the lat and long as imputs
-    #otherwise I don't think we need to change anything else about this
 
-    #key = month, val = temp in celcius
+    #the "weather_info" json object should return monthly weather data given the lat and long as imputs
     weather_data = {}
     
     url_weather = f"https://api.meteostat.net/v2/point/climate?lat={lat_str}&lon={long_str}&alt=58&x-api-key={weather_api_key}"
@@ -51,18 +56,23 @@ def get_weather_data(cur, conn, location):
     for month in data_json:
         #gets month of interest, 1 = Jan, 2 = Feb, 3 = March, etc.
         month_num_string = month['month']
-        #gets average of month in celcius
         temp_string_celcius = month['tavg']
         percip = month['prcp']
   
         weather_data[month_num_string] = (temp_string_celcius, percip) #, pressure, sunshine
   
-    
     return weather_data
     
 
 def weather_table(data, cur, conn, location, temp_list):
-    cur.execute(f'CREATE TABLE IF NOT EXISTS WeatherData (location INTEGER, average_temp_id INTEGER, average_precipitation_id INTEGER)') #, avg_pressure INTEGER, avg_hours_sunshine INTEGER
+    '''This function takes in a dictionary of the weather data, the cursor, connection, city name, and list of already seen temperature value
+    in the data dictionary. This creates a table for the Weather Data and then gets the temperature and precipitation from the data dictionary
+    for each month. It takes the average of these 12 months and inserts the id for the temperature into the table and the value for
+    precipitation into the table along with the city name (location). If a new temperature value was found it adds it to the temp_list
+    and creates a new id in the Temperatures table. Otherwise, it uses an existing id from that table to describe the avergae temperature
+    for the current location being used. It commits to the database and returns nothing.'''
+
+    cur.execute(f'CREATE TABLE IF NOT EXISTS WeatherData (location TEXT, average_temperature_id INTEGER, average_precipitation INTEGER)') 
     temp = 0
     percip = 0
   
@@ -81,11 +91,14 @@ def weather_table(data, cur, conn, location, temp_list):
     else:
         final_temp = temp_list.index(avg_temp)
 
- 
-    cur.execute(f'INSERT INTO WeatherData (location, average_temp_id, average_precipitation_id) VALUES (?,?,?)', (location, final_temp, avg_percip)) #, avg_pressure, avg_sunshine
+    cur.execute(f'INSERT INTO WeatherData (location, average_temperature_id, average_precipitation) VALUES (?,?,?)', (location, final_temp, avg_percip)) 
     conn.commit()
 
 def website_prep():
+    '''This function takes in no paramters. It preps the website data to be extracted easily later. It makes a request using the
+    happiest places to live url and then uses beautiful soup to find the table that we will extract data from in a later function.
+    It returns a list of all of the values within the table.''' 
+
     url = 'https://wallethub.com/edu/happiest-places-to-live/32619'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'}
 
@@ -99,6 +112,11 @@ def website_prep():
 
 
 def get_website_data(i, row_tags):
+    '''This function takes in the index for the current city being looked at and the row_tags data list of the table information from the
+    website that was found in the website_prep function. It looks at the specific row (i) in the list (row_tags) to find the overall rank,
+    city name, total happiness score, emotional/physical score, income/employment score, and community/environment score. It then add these 6 values to a list and 
+    returns that list. '''
+
     tr_tag = row_tags[i]
     td_tags = tr_tag.find_all("td")
     overall_rank = int(td_tags[0].text.strip())
@@ -113,11 +131,21 @@ def get_website_data(i, row_tags):
     
 
 def website_table(data, cur, conn):
+    '''This function takes in the list of data (the 6 values) from the website, the cursor, and connection. It creates a table for the website
+    data (HappyData) if one does not already exist. It then inserts the data into each column of the table. It commits the values to the
+    database and returns nothing. '''
+
     cur.execute('CREATE TABLE IF NOT EXISTS HappyData (overall_rank INTEGER, city TEXT, total_score INTEGER, well_being_rank INTEGER, income_employment_rank INTEGER, community_environment_rank INTEGER)')
     cur.execute('INSERT INTO HappyData (overall_rank, city, total_score, well_being_rank, income_employment_rank, community_environment_rank)  VALUES (?,?,?,?,?,?)', (data[0], data[1], data[2], data[3], data[4], data[5]))
     conn.commit()
 
 def get_start_index(cur, conn):
+    '''This function takes in the cursor and connection. This function finds where to start gathering data from in the next round of 
+    data extraction. It creates the HappyData table if it does not exist already. It then finds all of the 
+    cities in the table currently. If that length is 0, then it returns 0 (we are in the first round of data extraction from the API/webstie.
+    If it is between 1 and 181, then we return the next value to be extracted (if we ended at 25 extractions, we return 26 to start at the next
+    one). If we are out of this range, then we are done collecting data and return -1.'''
+
     cur.execute('CREATE TABLE IF NOT EXISTS HappyData (overall_rank INTEGER, city TEXT, total_score INTEGER, well_being_rank INTEGER, income_employment_rank INTEGER, community_environment_rank INTEGER)')
     cur.execute('SELECT city FROM HappyData')
     x = cur.fetchall()
@@ -130,6 +158,10 @@ def get_start_index(cur, conn):
     
 
 def get_temp_lists(cur):
+    '''This function takes in the cursor. It creates the Temperatures table if it does not already exist and selects the temperature values from it.
+    It then adds them all to a list and returns that list. This is meant to be used for figuring out the id of temperatures that will
+    be extracted from the API and added to the WeatherData table. '''
+
     cur.execute('CREATE TABLE IF NOT EXISTS Temperatures (temperature INTEGER, id INTEGER)')
     cur.execute('SELECT temperature FROM Temperatures')
     x = cur.fetchall()
@@ -139,7 +171,15 @@ def get_temp_lists(cur):
     return t
 
 def main():
-    cur, conn = create_connection("finalData1.db")
+    '''The main function takes in no parameters. It calls the create_connection function on the database name being used. It then creates start and
+    stop variables to make sure we don't extract more than 25 things from the APIs and website. Then it calls website_prep and get_temps_list. 
+    Then, it does a while loop that continues until around 25 things have been added to the tables. In the loop, we call get_start_index to 
+    get the starting point for data extraction. We make sure if it is -1, we do the calculations (see other python file) and then end the running
+    of the code. Then, we call get_website_data, get_weather_data, weather_table, and website_table to get the data and add it to the database.
+    Then we add one to our starting value for counting how many times we have done a data extraction, and we loop again. This
+    function returns nothing. '''
+
+    cur, conn = create_connection("finalProjectDatabase.db")
 
     start = 1
     stop = 25
@@ -149,15 +189,15 @@ def main():
     while start < stop:
         current_index = get_start_index(cur, conn)
         if current_index == -1:
-            #calcs_vis.main()
+            calcs_vis.main()
+            cur.close()
             break
 
         location = get_website_data(current_index, row_tags) #list of current location's data from website table
         weather_data = get_weather_data(cur, conn, location[1])
         weather_table(weather_data, cur, conn, location[1], temp_list)
-        website_table(location, cur, conn) #maybe make location the current_index here to reduce database duplicate data
-        # location_table(cur, conn, location[1], current_index)
-
+        website_table(location, cur, conn) 
+       
         start += 1
 
 
